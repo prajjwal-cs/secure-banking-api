b0f9e42c867d0e205024cac7ad47dee462632b06
prajjwal <prajjwalpachauri725@gmail.com>
2026-02-18 16:58:01 +0530

    Implemented JWT token provider

--- src/main/java/com/prajjwal/securebanking/security/JwtProvider.java
+++ src/main/java/com/prajjwal/securebanking/security/JwtProvider.java
@@ -1,4 +1,62 @@
 package com.prajjwal.securebanking.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Component
 public class JwtProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecretKey;
+
+    @Value("${app.jwt-expiration-milliseconds}")
+    private long jwtExpirationTime;
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecretKey));
+    }
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+
+        Date currentDate = new Date();
+        Date expirationDate = new Date(currentDate.getTime() + jwtExpirationTime);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(expirationDate)
+                .issuer("auth-bank")
+                .signWith(key())
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith((SecretKey) key())
+                    .build()
+                    .parse(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
