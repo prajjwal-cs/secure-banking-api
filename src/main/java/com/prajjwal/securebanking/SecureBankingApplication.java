f49026c23c70ea4000fd485e991229a090977987
prajjwal <prajjwalpachauri725@gmail.com>
2026-02-18 10:55:12 +0530

    Initial commit

---
+++ src/main/java/com/prajjwal/securebanking/SecureBankingApplication.java
@@ -0,0 +1,13 @@
+package com.prajjwal.securebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureBankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prajjwal/securebanking/SecureBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prajjwal.securebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
