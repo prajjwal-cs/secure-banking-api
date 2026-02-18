20c6d1b643b35a9181c65681d0f812a2f296df73
prajjwal <prajjwalpachauri725@gmail.com>
2026-02-18 18:06:58 +0530

    Implemented authentication entrypoint

--- src/main/java/com/prajjwal/securebanking/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/prajjwal/securebanking/security/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,27 @@
 package com.prajjwal.securebanking.security;
 
-public class JwtAuthenticationEntryPoint {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write("""
+                {
+                    "error": "Unauthorized",
+                    "message": "Authentication required to access this resource"
+                }
+                """
+        );
+    }
 }
